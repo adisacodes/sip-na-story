a764399443e09b0da05b387d09fa5c9065a9bdac
adisacodes <marian.adisa@student.moringaschool.com>
2026-06-29 00:09:38 +0300

    adjust navbar and made hero image full width banner

--- app.py
+++ app.py
@@ -2,17 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# ---------------- PAGE ROUTES ----------------
 
 @app.route("/")
 def homepage():
-    """Homepage: hero section, intro to The Velvet Still"""
     return render_template("home.html")
 
 
 @app.route("/menu")
 def menu_page():
-    """Service page: cocktail menu"""
     return render_template("menu.html")
 
 
